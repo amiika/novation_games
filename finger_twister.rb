415b211138064d29606eab3f5dec4de4835dc769
amiika <miika.alonen@vantaa.fi>
2021-05-03 22:08:38 +0300

    Added nyan cat animator and finger twister

--- finger_twister.rb
+++ finger_twister.rb
@@ -1,10 +1,17 @@
-# Easy template for creating a new game for Novation Launchpad and Sonic Pi.
-# If you create a new one please share it here so everyone can enjoy!
+# Finger twister for Novation Launchpad Mini and Sonic Pi
 
-# This simplified example "paints" random colors
+# Try to bend your fingers to same color spots. No points for now, just for fun.
+
+set :players, 2
+set :fingers, 5
 
 use_debug false
 use_midi_logging false
+
+# Randomize seed and starting point
+use_random_seed = Time.now.to_i
+SecureRandom.random_number(1000).times { rand }
+print rand
 
 # Change here for your inputs and outputs. See Sonic Pi's Prefs/IO for ports.
 launchpad_in = "/midi:midiin2_(lpminimk3_midi)_1:1/*"
@@ -84,6 +91,19 @@ define :set_cell_color do |x, y, rgb|
 end
 
 # Set multiple cells
+define :clear_pads do
+  pad_colors = []
+  8.times do |x|
+    8.times do |y|
+      cell = (x+1).to_s+(y+1).to_s
+      cell_color = [0x00, cell, 0]
+      pad_colors = pad_colors+cell_color
+    end
+  end
+  led_sysex pad_colors
+end
+
+# Set multiple cells
 define :set_colors do |arr, rgb|
   pad_colors = []
   arr.each do |cell|
@@ -91,6 +111,30 @@ define :set_colors do |arr, rgb|
     pad_colors = pad_colors+cell_color
   end
   led_sysex pad_colors
+end
+
+# Set colors for the whole matrix
+define :set_pad_colors_from_list do |matrix,list,s=0|
+  pad_colors = []
+  matrix.length.times do |x|
+    row = matrix[x]
+    row.length.times do |y|
+      cell = matrix[x][y]
+      cell_color = [0x03, ((matrix.length-x).to_s+(y+1).to_s).to_i, *list[(y+ (s==0 ? (x*row.length) : (x+s)))%list.length]]
+      pad_colors = pad_colors+cell_color
+    end
+  end
+  led_sysex pad_colors
+end
+
+# Defines color gradient array using sin
+define :get_color_gradient do |fr1, fr2, fr3, ph1, ph2, ph3, len=50, center=128, width=127|
+  colors = (1..len).map do |i|
+    red = [(Math.sin(fr1*i+ph1) * width + center).to_i,255].min
+    grn = [(Math.sin(fr2*i*ph2) * width + center).to_i,255].min
+    blue = [(Math.sin(fr3*i*ph3) * width + center).to_i,255].min
+    rgb(red,grn,blue)
+  end
 end
 
 # Set colors for the whole matrix
@@ -107,6 +151,14 @@ define :set_pad_colors do |matrix,rgb|
   led_sysex pad_colors
 end
 
+# Get player color
+define :player_color do |p|
+  # TODO / OPTIONAL: Color choosing from board?
+  colors = [45,76,74,72,100,95,2,103]
+  colors[p]
+end
+
+
 # Get sync type from the midi call
 define :sync_type do |address|
   v = get_event(address).to_s.split(",")[6]
@@ -122,13 +174,35 @@ define :start_game do
   stop_text # Stop texts if running
   set_programmer_mode # Set programmer mode
   $board = (1..8).map {|x| (1..8).map {|y| {x: x, y: y, value: nil}}} # Create new board
-  set_pad_colors $board, rgb(0,0,0) # Set color
+  print $board
+  clear_pads
+  players = get :players
+  $times = (1..players).map {|p| 0.0 }
   set :state, :relax
   set :game_over, false
+  
+  in_thread do
+    use_random_seed = Time.now.to_i
+    SecureRandom.random_number(1000).times { rand }
+    players = get :players
+    spots = players * 5
+    spots.times do |i|
+      x = rrand_i(0,7)
+      y = rrand_i(0,7)
+      
+      until $board[x][y][:value]==nil
+        x = rrand_i(0,7)
+        y = rrand_i(0,7)
+      end
+      
+      $board[x][y][:value] = (i%players)
+      set_cell_color_from_palette x+1, y+1, player_color(i%players)
+      
+      sleep 1
+    end
+  end
+  
 end
-
-# Start a new game
-start_game
 
 # Thread for listening events from the novation launchpad
 live_loop :event_listener do
@@ -145,10 +219,10 @@ live_loop :event_listener do
   
   if type=="note_on"
     if touch==0 # Touch off
-      cue :push_event, type: :push, x: x, y: y
+      cue :push, type: :off, x: x, y: y
       # sleep 0.5
     else
-      
+      cue :push, type: :on, x: x, y: y
     end
   elsif type=="control_change"
     if get_pad_status(19) and get_pad_status(91) then
@@ -173,20 +247,17 @@ end
 
 
 # Thread for keeping up the score
-live_loop :game do
+live_loop :fingertwister do
   use_real_time
-  event = sync :push_event
+  event = sync :push
   game_over = get :game_over
   
   if event
     x = event[:x]
     y = event[:y]
     
-    if event[:type]==:push
-      # Get board value for this push
-      cell_value = $board[x-1][y-1][:value]
+    if event[:type]==:on
       # Do something
-      set_cell_color_from_palette x, y, rrand_i(1,127)
     end
     
     if game_over
@@ -204,15 +275,15 @@ live_loop :game do
 end
 
 # Set different scales for different states
-relax = (scale :c4, :kumoi).shuffle
+relax = (scale :d3, :kumoi).shuffle
 happy = (scale :a4, :major_pentatonic).shuffle
 
 live_loop :music do
   state = get :state
   if state == :relax then
-    synth :kalimba, amp: 1.5, note: relax.tick if state == :relax
-    synth :dull_bell, amp: 1.5, note: happy.tick if state == :happy
-    sleep [0.25,0.5].choose
+    synth :dull_bell, note: relax[tick]
   end
-  relax.shuffle if rand<0.5
+  sleep 1
 end
+
+start_game
